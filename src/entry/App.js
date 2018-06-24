b05ead04cfaaf3c695d4365f73363d992c922e4b
Kyaw Htet Soe <kyaw.htetsoe@myrepublic.net>
2018-06-25 02:27:11 +0800

    deleted previous redux setup, will do later, put App.js into entry folder which later will group react-router and store configurations

---
+++ src/entry/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import LoginPage from "./login-page";
+import SignupPage from '../containers/signup-page';
+// import LoginPage from "./login-page";
+
+const App = () => (
+  <div className="App">
+    {/* <LoginPage /> */}
+    <SignupPage />
+  </div>
+);
+
+export default App;
