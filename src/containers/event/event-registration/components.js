d9884e1a72f5138282ed92705494a6438f2cf489
s10142884 <ambialynn@gmail.com>
2018-08-12 16:31:28 +0800

    create event registration files

---
+++ src/containers/event/event-registration/components.js
@@ -0,0 +1,222 @@
+import React from 'react';
+import {
+  Tabs, Button, Form, Input,
+} from 'antd';
+import { EVENT_EDIT } from '../../../actions/location';
+import {
+  TabIcon,
+  BoldText,
+  FullWidthTable,
+  FullButton,
+} from '../shared-styled';
+import { layout } from '../shared-components';
+
+const FormItem = Form.Item;
+const { TabPane } = Tabs;
+const readOnlyInput = {
+  style: {
+    border: 0,
+    outline: 0,
+    borderRadius: 0,
+    padding: 0,
+    borderBottom: '1px solid rgba(0, 0, 0, 0.2)',
+  },
+  readOnly: true,
+  size: 'small',
+};
+
+/* eslint react/prop-types: 0 */
+// ALL FORM ITEM MUST PASS IN decorator!
+export const EventViewTabs = ({ onChange, tabContents, props }) => {
+  const tabTitles = {
+    tab1: (
+      <BoldText>
+        <TabIcon type="calendar" />
+        Event
+      </BoldText>
+    ),
+    tab2: (
+      <BoldText>
+        <TabIcon type="calendar" />
+        Registration List
+      </BoldText>
+    ),
+  };
+
+  const EventPage = tabContents[0];
+  const RegistrationListPage = tabContents[1];
+
+  return (
+    <Tabs onChange={onChange} type="card">
+      <TabPane tab={tabTitles.tab1} key="tab1">
+        <EventPage {...props} />
+      </TabPane>
+      <TabPane tab={tabTitles.tab2} key="tab2">
+        <RegistrationListPage {...props} />
+      </TabPane>
+    </Tabs>
+  );
+};
+
+export const EventData = ({ decorator }) => (
+  <Form>
+    <FormItem>
+      {decorator('photoLink', { valuePropName: 'src' })(
+        <img
+          alt="example"
+          style={{
+            width: 'auto',
+            height: '200px',
+            margin: '0 auto 0 auto',
+            display: 'block',
+          }}
+        />,
+      )}
+    </FormItem>
+
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Event Id">
+      {decorator('id')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Event Name">
+      {decorator('name')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Event Description">
+      {decorator('description')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Start Date/Time">
+      {decorator('startDate')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="End Date/Time">
+      {decorator('endDate')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Location">
+      {decorator('location')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Postal Code">
+      {decorator('locationPostalCode')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Event Status">
+      {decorator('eventStatus')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Ticket Fee (SGD)">
+      {decorator('ticketFee')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="No of Pax">
+      {decorator('noOfPax')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem
+      {...layout}
+      style={{ marginBottom: 0 }}
+      label="Refreshment Provided"
+    >
+      {decorator('isRefreshmentProvided')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Contact Person">
+      {decorator('contactPerson')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Email Address">
+      {decorator('emailAddress')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Mobile No">
+      {decorator('mobilePhone')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Created By">
+      {decorator('createdBy')(<Input {...readOnlyInput} />)}
+    </FormItem>
+    <FormItem {...layout} style={{ marginBottom: 0 }} label="Created Date">
+      {decorator('createdDate')(<Input {...readOnlyInput} />)}
+    </FormItem>
+  </Form>
+);
+
+export const EditEventButton = ({ eventId }) => (
+  <FullButton type="primary" href={EVENT_EDIT.concat('/').concat(eventId)}>
+    Edit Event
+  </FullButton>
+);
+
+export const ShareFacebookButton = () => (
+  <Button icon="facebook" shape="circle" type="primary" ghost />
+);
+
+export const NotifyMsgButton = ({ onClickNotify, loading }) => (
+  <Button
+    icon="message"
+    shape="circle"
+    type="primary"
+    ghost
+    onClick={onClickNotify}
+    loading={loading}
+  />
+);
+
+export const RegistrationTable = ({
+  registrationList,
+  rowSelection,
+  onChange,
+  sortedInfo,
+  filteredInfo,
+}) => {
+  const columns = [
+    // dataIndex = databases column names
+    {
+      title: 'No',
+      dataIndex: 'no',
+      key: 'no',
+      width: '4%',
+      render: (text, record, index) => <span>{`${index + 1}`}</span>,
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      filteredValue: filteredInfo.name || null,
+      onFilter: (value, record) => record.name.toLowerCase().includes(value),
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
+      width: '19%',
+    },
+    {
+      title: 'Email Address',
+      dataIndex: 'emailAddress',
+      key: 'emailAddress',
+      sorter: (a, b) => a.emailAddress.length - b.emailAddress.length,
+      sortOrder: sortedInfo.columnKey === 'emailAddress' && sortedInfo.order,
+      width: '28%',
+    },
+    {
+      title: 'Mobile No',
+      dataIndex: 'mobilePhone',
+      key: 'mobilePhone',
+      width: '19%',
+    },
+    {
+      title: 'No. of Ticket(s)',
+      dataIndex: 'noOfPax',
+      key: 'noOfPax',
+      sorter: (a, b) => Number.parseInt(a.noOfPax, 10) - Number.parseInt(b.noOfPax, 10),
+      sortOrder: sortedInfo.columnKey === 'noOfPax' && sortedInfo.order,
+      width: '14%',
+    },
+    {
+      title: 'Payment',
+      dataIndex: 'paymentType',
+      key: 'paymentType',
+      sorter: (a, b) => a.paymentType.length - b.paymentType.length,
+      sortOrder: sortedInfo.columnKey === 'paymentType' && sortedInfo.order,
+      width: '15%',
+    },
+  ];
+
+  return (
+    <FullWidthTable
+      columns={columns}
+      dataSource={registrationList}
+      rowSelection={rowSelection}
+      onChange={onChange}
+      bordered
+      size="small"
+      pagination={{ position: 'top' }}
+    />
+  );
+};
---
+++ src/containers/event/event-registration/event-registration-page.js
@@ -0,0 +1,189 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { connect } from 'react-redux';
+import {
+  Form, Row, Col, message,
+} from 'antd';
+
+import { SUCCESS_NOTIFYEVENT, SHOWFOR } from '../../../actions/message';
+import {
+  TIME_FORMAT_DB,
+  DATE_FORMAT,
+  DEFAULT_DATE,
+  DEFAULT_TIME,
+  BASE_URL,
+} from '../../../actions/constants';
+import { PUBLIC_EVENT_VIEW } from '../../../actions/location';
+import {
+  EventData,
+  EditEventButton,
+  ShareFacebookButton,
+  NotifyMsgButton,
+} from './components';
+import { EventCard } from '../shared-styled';
+import { BackButton } from '../shared-components';
+import { postNotifyEvent } from '../../../reducers/eventmgmt/eventmgmt-data';
+
+class EventPage extends Component {
+  componentDidUpdate(prevProps) {
+    const {
+      eventmgmtData: { isPostApiLoading, postErrMsg },
+    } = this.props;
+
+    const isApiPost = prevProps.eventmgmtData.isPostApiLoading && !isPostApiLoading;
+    if (!isApiPost) return;
+
+    if (postErrMsg) {
+      message.error(postErrMsg, SHOWFOR);
+    } else {
+      message.success(SUCCESS_NOTIFYEVENT, SHOWFOR);
+    }
+  }
+
+  onClickNotify = () => {
+    const {
+      form: { getFieldValue },
+      performNotifyEvent,
+    } = this.props;
+    const id = getFieldValue('id');
+    const url = BASE_URL.concat(PUBLIC_EVENT_VIEW, '/', id);
+    performNotifyEvent({ id, url });
+  };
+
+  render() {
+    const {
+      form: { getFieldDecorator },
+      eventmgmtData: { isPostApiLoading },
+    } = this.props;
+    const shareColLayout = {
+      xs: { span: 24, offset: 0 },
+      sm: { span: 24, offset: 0 },
+      md: { span: 24, offset: 0 },
+      lg: { span: 12, offset: 0 },
+      xl: { span: 12, offset: 0 },
+      style: { marginBottom: 14 },
+    };
+    const actionColLayout = {
+      xs: { span: 24 },
+      sm: { span: 24 },
+      md: { span: 24 },
+      lg: { span: 12 },
+      xl: { span: 12 },
+      style: { marginBottom: 14 },
+    };
+
+    return (
+      <div>
+        <EventCard style={{ borderRadius: 15, margin: '0 auto 0 auto' }}>
+          <EventData decorator={getFieldDecorator} />
+          <br />
+          <Row gutter={8}>
+            <Col
+              {...shareColLayout}
+              {...{
+                lg: { span: 6, offset: 6 },
+                xl: { span: 6, offset: 6 },
+              }}
+            >
+              <ShareFacebookButton /> Share on facebook
+            </Col>
+            <Col {...shareColLayout}>
+              <NotifyMsgButton
+                onClickNotify={this.onClickNotify}
+                loading={isPostApiLoading}
+              />{' '}
+              Notify Club Members
+            </Col>
+          </Row>
+          <Row gutter={8}>
+            <Col {...actionColLayout}>
+              <EditEventButton eventId={this.eventId} />
+            </Col>
+            <Col {...actionColLayout}>
+              <BackButton />
+            </Col>
+          </Row>
+        </EventCard>
+      </div>
+    );
+  }
+}
+
+EventPage.propTypes = {
+  form: PropTypes.shape({}).isRequired,
+  performNotifyEvent: PropTypes.func.isRequired,
+  eventmgmtData: PropTypes.shape({}).isRequired,
+};
+
+const mapStateToProps = state => ({
+  eventmgmtData: state.eventmgmt.data,
+});
+
+const formatDate = (strDate) => {
+  if (strDate) {
+    const date = moment(new Date(strDate)).format(DATE_FORMAT) === DEFAULT_DATE
+      ? '/'
+      : moment(new Date(strDate))
+        .format(DATE_FORMAT)
+        .toString();
+    const time = moment(new Date(strDate)).format(TIME_FORMAT_DB) === DEFAULT_TIME
+      ? '/'
+      : moment(new Date(strDate))
+        .format(TIME_FORMAT_DB)
+        .toString();
+    return date.concat(' ', time);
+  }
+  return null;
+};
+
+const mapPropsToFields = ({
+  computedMatch: { params },
+  eventmgmtData: { eventsData },
+}) => {
+  const { id } = params;
+  const eventData = eventsData ? eventsData.find(item => item.id === id) : {};
+
+  return {
+    photoLink: Form.createFormField({ value: eventData.photoLink }),
+    id: Form.createFormField({ value: eventData.id }),
+    name: Form.createFormField({ value: eventData.name }),
+    description: Form.createFormField({ value: eventData.description }),
+    location: Form.createFormField({
+      value: `${eventData.locationLine1} ${eventData.locationLine2}`,
+    }),
+    locationPostalCode: Form.createFormField({
+      value: eventData.locationPostalCode,
+    }),
+    ticketFee: Form.createFormField({ value: eventData.ticketFee }),
+    noOfPax: Form.createFormField({ value: eventData.noOfPax }),
+    isRefreshmentProvided: Form.createFormField({
+      value: eventData.isRefreshmentProvided === '1' ? 'Yes' : 'No',
+    }),
+    contactPerson: Form.createFormField({ value: eventData.contactPerson }),
+    emailAddress: Form.createFormField({ value: eventData.emailAddress }),
+    mobilePhone: Form.createFormField({ value: eventData.mobilePhone }),
+    eventStatus: Form.createFormField({
+      value: eventData.eventStatus === '1' ? 'Open' : 'Closed',
+    }),
+    startDate: Form.createFormField({
+      value: formatDate(eventData.startDate),
+    }),
+    endDate: Form.createFormField({
+      value: formatDate(eventData.endDate),
+    }),
+    createdBy: Form.createFormField({ value: eventData.createdBy }),
+    createdDate: Form.createFormField({ value: eventData.createdDate }),
+  };
+};
+
+const mapDispatchToProps = {
+  performNotifyEvent: postNotifyEvent,
+};
+
+const FormEventPagePage = Form.create({ mapPropsToFields })(EventPage);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(FormEventPagePage);
